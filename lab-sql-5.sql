cfc827098c709c1b11865c7e60c5aa07f17fad65
Kirstin Gosling <kirst_gosling@yahoo.com>
2023-02-25 11:57:20 +0100

    start lab 5

---
+++ lab-sql-5.sql
@@ -0,0 +1,16 @@
+USE sakila;
+
+
+
+SELECT DISTINCT active from customer;
+
+SELECT *
+FROM customer
+WHere active = 0;alter
+
+CREATE TABLE backup table (
+
+
+);
+
+INSERT INTO backup_table values(16, 2, "SANDRA", "MARTIN"....), (64, 2, "JUDITH",    #can insert multiple rows using same 
